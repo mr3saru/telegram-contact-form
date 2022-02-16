55761ec70e48c5ad25dafb6fd4eefa0a16c0f34a
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-02-16 14:07:44 -0300

    Fix web process failed to bind to $PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 const token = process.env.TOKEN;
 const chatId = process.env.CHAT_ID;
