cc9175a0e4c859babc8206ade76b0b213d6d1d65
Rita Burrows <rita.burrows@cloudworks.cc>
2023-12-06 16:05:24 +0000

    adding validation for api/form

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const validateForm = require('./formValidator');
+const { errors } = require('celebrate');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -26,7 +28,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/api/form', jsonParser, function (req, res) {
+app.post('/api/form', jsonParser, validateForm, function (req, res) {
   const { name, contact, message } = req.body
   const text = `CONTACT FORM \nName: ${name} \nContact: ${contact} \nMessage: ${message}`
   bot.sendMessage(chatId, text).then(() => {
@@ -34,6 +36,8 @@ app.post('/api/form', jsonParser, function (req, res) {
       res.sendStatus(200);
     });
 })
+
+app.use(errors());
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
